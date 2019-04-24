b0e3159ef5e10fe303e163df7c6e1b4fcfd4fe65
Christopher Harrison <cnharrison@gmail.com>
2019-04-23 23:38:44 -0400

    add rss-parser-browser, getURl method. CORS blocked

--- src/components/BlogAggregator.js
+++ src/components/BlogAggregator.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
+import axios from "axios";
+var parser = require("rss-parser-browser");
 
 class BlogAggregator extends Component {
   getUrl = () => {
+    axios.get("https://sleeplady.com/feed").then(response => console.log(response));
+    // parser.parseURL("https://www.reddit.com/.rss", function(err, parsed) {
+    //   console.log(parsed.feed.title);
+    //   parsed.feed.entries.forEach(function(entry) {
+    //     console.log(entry.title + ":" + entry.link);
+    //   });
+    // });
   };
 
   render() {
-    return<div className="BlogAggregator">
-    </div>
+    return (
+      <div className="BlogAggregator">
+        <button onClick={this.getUrl}>test me</button>
+      </div>
+    );
   }
 }
 
