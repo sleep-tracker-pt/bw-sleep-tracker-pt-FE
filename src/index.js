2ad392f962092515a4ae421016dd0cd424decb19
Christopher Harrison <cnharrison@gmail.com>
2019-04-23 20:20:33 -0400

    add redux-logger, applyMiddleware thunk and logger

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 import "./index.css";
 import App from "./App";
 import reducer from "./reducers";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
