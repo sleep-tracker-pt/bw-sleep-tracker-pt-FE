877c4ac1143728fe09de787d8d916f1830589a70
Christopher Harrison <cnharrison@gmail.com>
2019-04-23 22:41:29 -0400

    add BlogAggregator component

---
+++ src/components/BlogAggregator.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class BlogAggregator extends Component {
+  render() {
+    return (
+      <div className="HomeContainer">
+        <BlogAggregator />
+      </div>
+    );
+  }
+}
+
+export default BlogAggregator;
--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import BlogAggregator from "./";
 
 const HomeContainer = () => {
-  return <div className="HomeContainer" />;
+  return (
+    <div className="HomeContainer">
+      <BlogAggregator />
+    </div>
+  );
 };
 
 export default HomeContainer;
