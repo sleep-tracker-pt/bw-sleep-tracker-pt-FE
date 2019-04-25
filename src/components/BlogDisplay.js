5189abac09df7741e56fe8ddf2e98deb68f7e018
Christopher Harrison <cnharrison@gmail.com>
2019-04-24 22:39:24 -0400

    fix broken blog image links w/ fillmurray

--- src/components/BlogDisplay.js
+++ src/components/BlogDisplay.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import { Media, Image } from "react-bootstrap";
 
-const BlogDisplay = props => {
-  return (
-    <Media>
-      <Image
-        width={128}
-        height={128}
-        src={props.thumbnailUrl}
-        alt="blog image"
-        thumbnail
-        fluid
-      />
-      {/* <img width={128} height={128} src={props.thumbnailUrl} alt="blog image" /> */}
-      <Media.Body>
-        <h5>{props.title}</h5>
-        <p>{props.body}</p>
-      </Media.Body>
-    </Media>
-  );
-};
+
+class BlogDisplay extends Component {
+  addHolder = event => (event.target.src = "https://www.fillmurray.com/128/128");
+
+  render() {
+    return (
+      <Media>
+        <Image
+          width={128}
+          height={128}
+          src={this.props.thumbnailUrl}
+          alt="blog image"
+          onError={this.addHolder}
+          thumbnail
+          fluid
+        />
+        <Media.Body>
+          <h5>{this.props.title}</h5>
+          <p>{this.props.body}</p>
+        </Media.Body>
+      </Media>
+    );
+  }
+}
 
 export default BlogDisplay;
