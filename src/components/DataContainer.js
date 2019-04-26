cb06d7d406870bbe15d7686058c1da679b405083
Christopher Harrison <cnharrison@gmail.com>
2019-04-25 22:29:26 -0400

    add recharts, set up example plot

---
+++ src/components/DataContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeMoodChart from "./HomeMoodChart";
+
+const DataContainer = () => {
+  return (
+    <div className="DataContainer">
+      <HomeMoodChart />
+    </div>
+  );
+};
+
+export default DataContainer;
--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import BlogAggregator from "./BlogAggregator";
+import DataContainer from "./DataContainer";
+
+import "./HomeMoodChart.css";
 
 const HomeContainer = () => {
   return (
     <div className="HomeContainer">
       <BlogAggregator />
+      <DataContainer />
     </div>
   );
 };
