c1cd50e089eba0f2b162b7ab71005de6a5a67e76
Christopher Harrison <cnharrison@gmail.com>
2019-04-26 15:28:07 -0400

    create skeleton StatsContainer

---
+++ src/components/StatsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StatsContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default StatsContainer;
