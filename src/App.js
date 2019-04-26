3e6770fcb6b9c44bdbb8070b85018b0234231ee9
Kristea Gamel <krgamel@gmail.com>
2019-04-25 22:02:24 -0500

    nav bar functional. zero styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import HomeContainer from "./components/HomeContainer";
-
+import NavBar from './components/navbar';
 import LoginPage from './components/LoginPage';
 
 import {Route, } from 'react-router-dom';
@@ -11,6 +11,7 @@ function App() {
   return (
    <div> 
     <div>
+      <NavBar />
       <Route path = "/login" component = {LoginPage} />
 
     </div>
