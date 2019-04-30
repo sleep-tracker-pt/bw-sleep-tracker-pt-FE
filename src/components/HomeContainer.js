6f5900e359b1a0a8cfd483b6ef2dd14733403d4e
Kristea Gamel <krgamel@gmail.com>
2019-04-30 14:44:02 -0500

    started outlining grid layout for homecontainer
    using styled components

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -2,16 +2,57 @@ import React from "react";
 import BlogAggregator from "./BlogAggregator";
 import HomeMoodChart from "./HomeMoodChart";
 import StatsContainer from "./StatsContainer";
+import styled from 'styled-components';
+
+
+const PageDiv= styled.div `
+
+
+
+`;
+const HeaderGroup= styled.div ``;
+
+const Title= styled.h3 `
+font-family: 'Open Sans', sans-serif; 
+font-weight: 800;
+color: #fff;
+font-size: 30px;
+margin: 10px;
+`;
+const ContainerDiv= styled.div ``;
+
+
+const Grid1Div= styled.div ``;
+
+
+const Grid2Div= styled.div ``;
+
+
+const Grid3Div= styled.div ``;
 
 
 const HomeContainer = () => {
   return (
-    <div className="HomeContainer">
-      <h1>Sleep Tracker Front End 😂👌💯</h1>
+    <PageDiv>
+    <HeaderGroup>
+      <Title>Welcome to your SleepTracker Dashboard</Title>
+    </HeaderGroup>
+    <ContainerDiv >
+
+    <Grid1Div >
       <BlogAggregator />
+    </Grid1Div>
+
+    <Grid2Div >
       <HomeMoodChart/>
+    </Grid2Div>
+
+    <Grid3Div> 
       <StatsContainer/>
-    </div>
+    </Grid3Div>
+
+    </ContainerDiv>
+    </PageDiv>
   );
 };
 
