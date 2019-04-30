82052ab3397a376c08fb2ff6c70ddb3c10aa0caf
Kristea Gamel <krgamel@gmail.com>
2019-04-30 18:42:07 -0500

    super basic layout on home container
    need to style each component

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -6,11 +6,20 @@ import styled from 'styled-components';
 
 
 const PageDiv= styled.div `
+display: grid;
+grid-gap: 30px;
+margin: 20px;
+grid-template-columns: repeat(8, [col] 150px );
+grid-template-rows: repeat(1, [row] auto );
+background-color: rgba(21, 78, 110, 0.7);
+color: #d3dce3;
 
 
 
 `;
-const HeaderGroup= styled.div ``;
+const HeaderGroup= styled.div `
+text-align: center;
+`;
 
 const Title= styled.h3 `
 font-family: 'Open Sans', sans-serif; 
@@ -19,27 +28,48 @@ color: #fff;
 font-size: 30px;
 margin: 10px;
 `;
-const ContainerDiv= styled.div ``;
 
 
-const Grid1Div= styled.div ``;
+
+const Grid1Div= styled.div `
+background-color: #0f2f5a
+border-radius: 5px;
+padding: 20px;
+font-size: 16px;
+grid-column: col / span 4;
+grid-row: row /span 20;
+`;
 
 
-const Grid2Div= styled.div ``;
+const Grid2Div= styled.div `
+background-color: #0f2f5a
+border-radius: 5px;
+padding: 20px;
+font-size: 16px;
+grid-column: col 5/ span 3;
+grid-row: row / span 20;
+`;
 
 
-const Grid3Div= styled.div ``;
+const Grid3Div= styled.div `
+background-color: #0f2f5a
+border-radius: 5px;
+padding: 20px;
+font-size: 16px;
+grid-column: col 8/ span 2;
+grid-row: row  / span 20;
+`;
 
 
 const HomeContainer = () => {
   return (
-    <PageDiv>
-    <HeaderGroup>
+
+    <>
+     <HeaderGroup>
       <Title>Welcome to your SleepTracker Dashboard</Title>
     </HeaderGroup>
-    <ContainerDiv >
-
-    <Grid1Div >
+    <PageDiv>
+     <Grid1Div >
       <BlogAggregator />
     </Grid1Div>
 
@@ -51,8 +81,10 @@ const HomeContainer = () => {
       <StatsContainer/>
     </Grid3Div>
 
-    </ContainerDiv>
+    
     </PageDiv>
+
+    </>
   );
 };
 
