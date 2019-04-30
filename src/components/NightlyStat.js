a52718ca92359a2d11a27309e4637b778d1f578f
Christopher Harrison <cnharrison@gmail.com>
2019-04-29 20:20:42 -0400

    add NightlyStat component, map, pass props

---
+++ src/components/NightlyStat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NightlyStat = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )}
+
+
+export default NightlyStat;
--- src/components/NightlyStats.js
+++ src/components/NightlyStats.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import NightlyStat from "./NightlyStat";
+import { directive } from "@babel/types";
 
 class NightlyStats extends Component {
   render() {
-    return <div />;
+    return (
+      <div className="nightly-stats">
+        {this.props.transformedSleepData.map(stat => {
+          return <NightlyStat transformedSleepData={stat} />;
+        })}
+      </div>
+    );
   }
 }
 
