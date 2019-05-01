66b2d10da3116d3294c1862b46039de567b9c84a
Kristea Gamel <krgamel@gmail.com>
2019-05-01 16:33:00 -0500

    dashboard and chart design

--- src/components/NightlyStats.js
+++ src/components/NightlyStats.js
@@ -2,15 +2,33 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import NightlyStat from "./NightlyStat";
 
+
+import styled from "styled-components";
+import  "../index.css";
+
+const NightStats = styled.div `
+text-align: center;
+`;
+const NightDiv = styled.div `
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+
+
+
+`;
 class NightlyStats extends Component {
   render() {
     return (
-      <div className="nightly-stats">
-        <h4>last 7 days</h4>
-        {this.props.filteredSleepData.map(stat => {
+      <NightStats>
+        <h4>Moods throughout the week</h4>
+        
+        <NightDiv>
+           {this.props.filteredSleepData.map(stat => {
           return <NightlyStat filteredSleepData={stat} />;
-        })}
-      </div>
+          })}
+        </NightDiv>
+      </NightStats>
     );
   }
 }
