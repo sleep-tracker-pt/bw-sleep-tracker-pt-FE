59ed482a7e8bdf44f13c99c575372f3199c5e510
Christopher Harrison <cnharrison@gmail.com>
2019-04-30 20:04:45 -0400

    add recommendedhours skeleton

---
+++ src/components/RecommendedHours.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RecommendedHours extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default RecommendedHours;
