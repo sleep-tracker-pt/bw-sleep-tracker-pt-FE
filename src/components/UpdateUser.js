5269b3c0831f14ad2ec157da6a2744f976400e09
Christopher Harrison <cnharrison@gmail.com>
2019-05-04 14:14:13 -0400

    resolve conflict

--- src/components/UpdateUser.js
+++ src/components/UpdateUser.js
@@ -3,63 +3,50 @@ import { connect } from "react-redux";
 import moment from "moment";
 import DatePicker from "react-date-picker";
 import styled from "styled-components";
-import {updateUser} from "../actions/";
 import account from "../img/account.svg";
 
-const AccountPage = styled.div `
-margin: 50px auto;
-width: 60%;
-${'' /* height: 500px; */}
-text-align: center;
-background: rgba(211, 220, 227, 0.5);
-`;
-
-const UserDiv = styled.div `
-margin: 50px auto;
-`;
-
-const UserName = styled.h3 ``;
-
-const Bday = styled.h4 ``;
-
-const BdayP = styled.p `
-
-`;
-
-const UpdateForm = styled.form ``;
-
-const NameInput = styled.input ``;
-
-const NewPasswordInput = styled.input ``;
-
-const DateDiv = styled.div ``;
-
-const CurrentPassword = styled.input ``;
-
-const ChangeButton = styled.button ``;
-const UpdateButton = styled.button ``;
-
-const UserDisplay = styled.div `
-margin: 50px auto;
-width: 60%;
-${'' /* height: 300px; */}
-background: rgba(211, 220, 227, 0.8);
-`;
-
-const ImgDiv = styled.div ``;
-
-
-
-
-
-
-<<<<<<< HEAD
-import { updateUser } from "../actions/"
-import { logout } from "../actions/";
-||||||| merged common ancestors
 import { updateUser } from "../actions/";
-=======
->>>>>>> 6c475c781870c86732b31e1760ffbd5fc3f995ca
+import { logout } from "../actions/";
+
+const AccountPage = styled.div`
+  margin: 50px auto;
+  width: 60%;
+  ${"" /* height: 500px; */}
+  text-align: center;
+  background: rgba(211, 220, 227, 0.5);
+`;
+
+const UserDiv = styled.div`
+  margin: 50px auto;
+`;
+
+const UserName = styled.h3``;
+
+const Bday = styled.h4``;
+
+const BdayP = styled.p``;
+
+const UpdateForm = styled.form``;
+
+const NameInput = styled.input``;
+
+const NewPasswordInput = styled.input``;
+
+const DateDiv = styled.div``;
+
+const CurrentPassword = styled.input``;
+
+const ChangeButton = styled.button``;
+const UpdateButton = styled.button``;
+
+const UserDisplay = styled.div`
+  margin: 50px auto;
+  width: 60%;
+  ${"" /* height: 300px; */}
+  background: rgba(211, 220, 227, 0.8);
+`;
+
+const ImgDiv = styled.div``;
 
 export class UpdateUser extends Component {
   state = {
@@ -97,43 +84,40 @@ export class UpdateUser extends Component {
     alert("user info has been updated, please login again to continue");
   };
 
-    updateOpener = () => {
-      this.setState({...this.state, clicked: true});
-    }
-    handleChange = date => {
-      this.setState({ birthDate: date });
-    };
-    render() {
-      
-      if (this.state.clicked) {
-        return (
-       
-          <AccountPage>
-           <ImgDiv>
-          <img src={account} width="50%"/>
-        </ImgDiv>
-            <UserDiv>
-              <UserName>Username:{this.props.userData.username}</UserName>
-              <Bday>Birthday: {this.props.userData.birthDate}</Bday>
-            </UserDiv>
+  updateOpener = () => {
+    this.setState({ ...this.state, clicked: true });
+  };
+  handleChange = date => {
+    this.setState({ birthDate: date });
+  };
+  render() {
+    if (this.state.clicked) {
+      return (
+        <AccountPage>
+          <ImgDiv>
+            <img src={account} width="50%" />
+          </ImgDiv>
+          <UserDiv>
+            <UserName>Username:{this.props.userData.username}</UserName>
+            <Bday>Birthday: {this.props.userData.birthDate}</Bday>
+          </UserDiv>
 
-            <UpdateForm onSubmit={this.updateHandler}>
+          <UpdateForm onSubmit={this.updateHandler}>
+            <NameInput
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.inputHandler}
+              placeholder="new username"
+            />
 
-              <NameInput 
-                type="text"
-                name="username"
-                value={this.state.username}
-                onChange={this.inputHandler}
-                placeholder="new username"
-              />
-
-              <NewPasswordInput 
+            <NewPasswordInput
               type="password"
               name="password"
               value={this.state.password}
               onChange={this.inputHandler}
               placeholder="new password"
-              />
+            />
             <BdayP>Please enter your Birthdate:</BdayP>
 
             <DateDiv>
@@ -142,32 +126,32 @@ export class UpdateUser extends Component {
                 value={this.state.birthDate}
                 clearIcon={null}
               />
-              </DateDiv>
+            </DateDiv>
 
-              <CurrentPassword 
-                type="password"
-                name="current password"
-                value={this.state.checkpassword}
-                onChange={this.inputHandler}
-                placeholder="current password"
-              />
-              <ChangeButton type="submit">Update Changes</ChangeButton>
-
-            </UpdateForm>
-          </AccountPage>
-          );
-      } else {
-        return (
-          <UserDisplay>
-             <UserName>Username:{this.props.userData.username}</UserName>
-              <Bday>Birthday: {this.props.userData.birthdate}</Bday>
-          <UpdateButton onClick={this.updateOpener}>Update {this.props.userData.username}</UpdateButton>
-          </UserDisplay>
-        );
-      }
+            <CurrentPassword
+              type="password"
+              name="current password"
+              value={this.state.checkpassword}
+              onChange={this.inputHandler}
+              placeholder="current password"
+            />
+            <ChangeButton type="submit">Update Changes</ChangeButton>
+          </UpdateForm>
+        </AccountPage>
+      );
+    } else {
+      return (
+        <UserDisplay>
+          <UserName>Username:{this.props.userData.username}</UserName>
+          <Bday>Birthday: {this.props.userData.birthdate}</Bday>
+          <UpdateButton onClick={this.updateOpener}>
+            Update {this.props.userData.username}
+          </UpdateButton>
+        </UserDisplay>
+      );
     }
   }
-
+}
 
 const mapStateToProps = state => {
   return {
