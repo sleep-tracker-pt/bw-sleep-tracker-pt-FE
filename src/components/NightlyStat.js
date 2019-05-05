aeda3b072af45a7a7db6a135fddbc94fc3bd6074
Christopher Harrison <cnharrison@gmail.com>
2019-05-05 19:51:09 -0400

    convet NightlyStat to class, add buttons to edit and delete sessions

--- src/components/NightlyStat.js
+++ src/components/NightlyStat.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import moment from "moment";
+import { Modal, Form, Button } from "react-bootstrap";
 
+class NightlyStat extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
 
-const NightlyStat = props => {
-  return (
-    <div>
-        <p>{moment(props.filteredSleepData.start).startOf('day').fromNow()}</p>
-        <p>to bed: {props.filteredSleepData.emojiBed}</p>
-        <p>at work: {props.filteredSleepData.emojiWork}</p>
-        <p>average: {props.filteredSleepData.emojiAverage}</p>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <p>
+          {moment(this.props.filteredSleepData.start)
+            .startOf("day")
+            .fromNow()}
+        </p>
+        <p>to bed: {this.props.filteredSleepData.emojiBed}</p>
+        <p>at work: {this.props.filteredSleepData.emojiWork}</p>
+        <p>average: {this.props.filteredSleepData.emojiAverage}</p>
+        <Button variant="outline-light" size="sm" block>
+          Edit
+        </Button>
+        <Button variant="outline-danger" size="sm" block>
+          Delete
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default NightlyStat;
